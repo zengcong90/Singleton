d3118297b65a8a78a2220e486419c4c2ebb32f3d
zengcong90 <38835689+zengcong90@users.noreply.github.com>
2018-07-05 17:09:12 +0800

    Update singletonA.java

--- singleton/src/singletonA.java
+++ singleton/src/singletonA.java
@@ -1,15 +1,15 @@
-public class singletonA {
+public class Singleton {
     //懒汉式单例模式（线程不安全）
-    private static singletonA instance;
+    private static Singleton instance;
     //增加一个记录对象次数的变量
     private static int count;
-    private singletonA(){
+    private Singleton(){
 
-        System.out.println("singletonA私有的构造方法被实例化"+ (++count) +"次");
+        System.out.println("Singleton私有的构造方法被实例化"+ (++count) +"次");
     }
-    public static singletonA getA( ){
+    public static Singleton getA( ){
             if(instance==null){
-                instance = new singletonA();
+                instance = new Singleton();
             }
         return instance;
     }
